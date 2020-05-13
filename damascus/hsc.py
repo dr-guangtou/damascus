c54fb1fde1b48aa054721f0fedd8944414634d1f
dr-guangtou <dr.guangtou@gmail.com>
2020-05-13 15:17:43 -0400

    Add doc

--- damascus/hsc.py
+++ damascus/hsc.py
@@ -18,8 +18,36 @@ __all__ = ['filter_hsc_fdfc_mask']
 
 HSC_ZP = 27.0  # Zeropoint for HSC survey
 
-def filter_hsc_fdfc_mask(cat, fdfc_mask, ra='RA', dec='DEC', verbose=True):
-    """Filter a catalog through HSC FDFC mask."""
+def filter_hsc_fdfc_mask(cat, fdfc_mask, ra='RA', dec='DEC', nest=True, verbose=False):
+    '''Filter a catalog through HSC FDFC mask."""
+
+    Parameters
+    ----------
+    cat: astropy.table or string
+         Input catalog of objects to filter. Either the actual catalog or the path to
+         the table.
+    fdfc_mask: healpy mask or string
+         Healpix mask. Either the mask itself or path to the mask file.
+    ra: string, optional
+         Column name for RA. Default: "RA".
+    dec: string, optional
+         Column name for Dec. Default: "DEC".
+    nest: bool, optional
+         If True, assume NESTED pixel ordering, otherwise, RING pixel ordering.
+         Default: True
+    verbose: bool, optional
+         Annouce progress. Default: False
+
+    Returns
+    -------
+    cat: astropy.table
+        Filtered through catalog.
+
+    Notes
+    -----
+        Will return None if no object is matched.
+
+    '''
     # Read the fits catalog if input is path to the file
     if isinstance(cat, str):
         cat = fits.open(cat, memmap=True)[1].data
@@ -44,7 +72,49 @@ def filter_hsc_fdfc_mask(cat, fdfc_mask, ra='RA', dec='DEC', verbose=True):
 def get_fdfc_borders(healpix_mask, nest=False, n_samples=10000,
                      distance=1.0, use_boundary=True, alpha=0.1,
                      verbose=True):
-    """Get the outer borders of each field in the FDFC region."""
+    '''Get the outer borders of each field in the FDFC region.
+
+    Parameters
+    ----------
+    healpix_mask: string
+         Path to the Healpix mask file.
+    nest: bool, optional
+         If True, assume NESTED pixel ordering, otherwise, RING pixel ordering.
+         Default: True
+    n_samples: int, optional
+         Size of the sub-sample used for separating the samples into differnt
+         contious fields. Default: 10000
+    distance: float, optional
+         Distance threshold for separating fields. In unit of degree.
+         Default: 1.0
+    use_boundary: bool, optional
+         Whether use the pixel boundaries. Default: True.
+    alpha: float, optional
+         Behavior of the alpha shape code. Default: 0.1
+    verbose: bool, optional
+         Annouce progress. Default: False
+
+    Returns
+    -------
+    borders: dict
+        Dictionary that keeps the border coordinates of each sub-field.
+
+    Examples
+    --------
+
+
+    Notes
+    -----
+        - Based on `alpha-shape` algorithm.
+            I steal some really nice code from StackOverflow by Iddo Hanniel.
+            You can find them here:
+            - https://stackoverflow.com/questions/23073170/calculate-bounding-polygon-of-alpha-shape-from-the-delaunay-triangulation
+            - https://stackoverflow.com/questions/50549128/boundary-enclosing-a-given-set-of-points
+        - `use_boundary`: will have 4x more coordinates, but is more accurate.
+        - `alpha`: the smaller the value, the concave hull will follow the distributions of
+          the points.
+
+    '''
     # Read in the healpix mask and get the NSIDE and pixel indices.
     mask = hp.read_map(healpix_mask, nest=nest)
     nside, hp_indices = hp.get_nside(mask), np.where(mask)[0]
